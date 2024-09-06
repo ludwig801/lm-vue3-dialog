6e8036a7bd13351c49c843a8959db28e1224a6ed
Luís Magalhães <lmaga801@gmail.com>
2024-09-06 13:52:46 +0100

    v0.1.6

---
+++ dist/lm-vue3-dialog.js
@@ -0,0 +1,75 @@
+import { defineComponent as w, mergeModels as g, useCssVars as y, useModel as b, ref as d, watch as h, onMounted as k, onBeforeUnmount as M, openBlock as D, createElementBlock as _, normalizeClass as u, createElementVNode as v, renderSlot as T } from "vue";
+const B = /* @__PURE__ */ w({
+  __name: "Dialog",
+  props: /* @__PURE__ */ g({
+    width: { default: () => "24rem" },
+    maxWidth: { default: () => "calc(100vw - 4rem)" },
+    animationTime: { default: () => 150 },
+    backdropClass: {},
+    contentClass: {}
+  }, {
+    open: { type: Boolean, required: !0 },
+    openModifiers: {}
+  }),
+  emits: /* @__PURE__ */ g(["update:open", "open:begin", "open:end", "close:begin", "close:end"], ["update:open"]),
+  setup(n, { emit: s }) {
+    y((e) => ({
+      f4993570: e.width,
+      "5203d800": e.maxWidth,
+      "946c0148": e.animationTime + "ms"
+    }));
+    const t = b(n, "open"), o = s, l = n, a = d(), i = d(!1), c = d(!1);
+    h(t, (e) => e ? C() : r());
+    function p(e) {
+      return new Promise((f) => setTimeout(f, Math.max(e, 0)));
+    }
+    async function C() {
+      var e;
+      try {
+        o("open:begin"), i.value = !0, (e = a.value) == null || e.showModal(), await p(l.animationTime), o("open:end");
+      } finally {
+        i.value = !1;
+      }
+    }
+    async function r() {
+      var e;
+      try {
+        o("close:begin"), c.value = !0, await p(l.animationTime - 10), (e = a.value) == null || e.close(), o("update:open", !1), o("close:end");
+      } finally {
+        c.value = !1;
+      }
+    }
+    function m(e) {
+      e.preventDefault(), e.stopImmediatePropagation(), i.value || r();
+    }
+    return k(() => {
+      var e;
+      (e = a.value) == null || e.addEventListener("cancel", m);
+    }), M(() => {
+      var e;
+      (e = a.value) == null || e.removeEventListener("cancel", m);
+    }), (e, f) => (D(), _("dialog", {
+      class: u(["dialog", { isClosing: c.value }]),
+      ref_key: "dialog",
+      ref: a
+    }, [
+      v("div", {
+        class: u(["dialog-backdrop", e.backdropClass]),
+        onClick: r
+      }, null, 2),
+      v("div", {
+        class: u(["dialog-content", e.contentClass])
+      }, [
+        T(e.$slots, "default", {}, void 0, !0)
+      ], 2)
+    ], 2));
+  }
+}), E = (n, s) => {
+  const t = n.__vccOpts || n;
+  for (const [o, l] of s)
+    t[o] = l;
+  return t;
+}, I = /* @__PURE__ */ E(B, [["__scopeId", "data-v-816b0135"]]);
+export {
+  I as LMDialog
+};
