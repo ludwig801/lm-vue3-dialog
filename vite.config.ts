294251835b8e760a7303339f86bad24f4bcdbd59
Luís Magalhães <lmaga801@gmail.com>
2024-09-06 17:26:14 +0100

    Styles are now auto-injected

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue(), libInjectCss(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/index.ts"),
@@ -21,5 +23,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), dts({ rollupTypes: true })],
 });
