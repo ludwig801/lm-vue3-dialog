5bed81e4d2d6e6f167c664e5af36c331c63cc98a
Luís Magalhães <lmaga801@gmail.com>
2024-09-06 18:01:52 +0100

    Replaced vite-plugin-dts with vue-tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), libInjectCss(), dts({ rollupTypes: true })],
+  plugins: [vue(), libInjectCss()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/index.ts"),
